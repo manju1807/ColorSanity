22ee8b78b662b0301c4309975104eb600bd95689
manju1807 <rmanjunath18@outlook.com>
2024-12-27 13:59:27 +0530

    feat: project init

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,39 +1,37 @@
 import { fileURLToPath } from "node:url";
-import { configDefaults, defineConfig, mergeConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 import viteConfig from "./vite.config";
 
-export default mergeConfig(
-	viteConfig,
-	defineConfig({
-		test: {
-			environment: "jsdom",
+export default defineConfig({
+	...viteConfig,
+	test: {
+		environment: "jsdom",
+		exclude: [
+			...configDefaults.exclude,
+			"e2e/**",
+			"node_modules/*",
+			".ignored_node_modules/**",
+		],
+		globals: true,
+		root: fileURLToPath(new URL("./", import.meta.url)),
+		coverage: {
+			provider: "v8",
+			reporter: ["text", "json", "html"],
 			exclude: [
-				...configDefaults.exclude,
-				"e2e/**",
-				"node_modules/*",
+				"coverage/**",
+				"dist/**",
+				"**/[.]**",
+				"packages/*/test?(s)/**",
+				"**/*.d.ts",
+				"cypress/**",
+				"test?(s)/**",
+				"test?(-*).?(c|m)[jt]s?(x)",
+				"**/*{.,-}{test,spec}.?(c|m)[jt]s?(x)",
+				"**/__tests__/**",
+				"**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
+				"**/node_modules/**",
 				".ignored_node_modules/**",
 			],
-			globals: true,
-			root: fileURLToPath(new URL("./", import.meta.url)),
-			coverage: {
-				provider: "v8",
-				reporter: ["text", "json", "html"],
-				exclude: [
-					"coverage/**",
-					"dist/**",
-					"**/[.]**",
-					"packages/*/test?(s)/**",
-					"**/*.d.ts",
-					"cypress/**",
-					"test?(s)/**",
-					"test?(-*).?(c|m)[jt]s?(x)",
-					"**/*{.,-}{test,spec}.?(c|m)[jt]s?(x)",
-					"**/__tests__/**",
-					"**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
-					"**/node_modules/**",
-					".ignored_node_modules/**",
-				],
-			},
 		},
-	}),
-);
+	},
+});
